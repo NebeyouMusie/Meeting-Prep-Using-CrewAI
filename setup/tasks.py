00a33e2bbe46b7975e5b79e79afe4c36529ccb0c
Nebeyou Musie <Nebeyoumusie@gmail.com>
2024-09-03 08:46:49 +0000

    Update tasks.py

--- setup/tasks.py
+++ setup/tasks.py
@@ -1,11 +1,3 @@
-import sys
-from pathlib import Path
-
-# Add the parent directory to the system path
-current_dir = Path(__file__).resolve().parent
-parent_dir = current_dir.parent
-sys.path.append(str(parent_dir))
-
 from textwrap import dedent
 from crewai import Task
 
